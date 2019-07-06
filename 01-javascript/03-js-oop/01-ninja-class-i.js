13148efeb5a020e4acf9cfda6fd17aac45894ca8
Blake Caldwell <blakecaldwell@MacBook-Pro.local>
2019-07-06 15:07:20 -0500

    ninja 3 done

--- 01-javascript/03-js-oop/01-ninja-class-i.js
+++ 01-javascript/03-js-oop/01-ninja-class-i.js
@@ -1,34 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ninja Class I</title>
+	<script type="text/javascript">
 
-class Ninja {
-    constructor(name, health, speed, strength){
-        this.name = name;
-        this.health = 100;
-        var speed = 3;
-        var strength = 3;
-    }
-    sayName() {
-        console.log("Hi! My name is " + this.name);
-    }
-    showStats() {
-        console.log(`Name: ${this.name} Health: ${this.health} Speed: ${this.speed} Strength: ${this.strength}`);
-    }
-    drinkSake() {
-        this.health += 10;
-    }
-}
+	function Ninja(name) {
+			this.name = name;
+			this.health = 100;
+			var speed = 3;
+			var strength = 3;
 
-class Sensei extends Ninja {
-    constructor(name) {
-      super(name);
-      this.health = 200;
-      this.speed = 10;
-      this.strength = 10;
-      this.widsom = 10;
-    }
-}
-// example output
-const superSensei = new Sensei("Master Splinter");
-superSensei.speakWisdom();
-// -> "What one programmer can do in one month, two programmers can do in two months."
-superSensei.showStats();
-// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
+			this.showStats = function() {
+				console.log("Name: " + this.name + ", Health: " + this.health + ", Speed: " + speed + ", Strength: " + strength);
+				return this;
+			}
+		}
+
+		Ninja.prototype.sayName = function() {
+			console.log("My name is " + this.name);
+			return this;
+		}
+
+		Ninja.prototype.drinkSake = function(){
+			this.health += 10;
+			return this;
+		}
+
+		const ninja1 = new Ninja("Goemon");
+		ninja1.sayName();
+		ninja1.showStats();
+
+
+	</script>
+</head>
+<body>
+
+</body>
+</html>
