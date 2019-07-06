3c31098f474f521090918b367e458f2a55fe97ef
Blake Caldwell <blakecaldwell@MacBook-Pro.local>
2019-07-06 13:08:00 -0500

    trivia done

---
+++ 01-javascript/02-js-fundamentals/02-hoisting.js
@@ -0,0 +1,28 @@
+//1
+undefined
+
+//2
+magnet
+
+//3
+super cool
+
+//4
+chicken
+half-chicken
+
+//5
+error: mean is not a function
+
+//6
+undefined
+rock
+r&b
+disco
+
+
+//7
+undefined
+seattle
+burbank
+undefined
