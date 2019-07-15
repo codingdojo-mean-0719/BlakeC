3e1f523c2a70b267c8ac18c7223464eeef4bc4ce
Blake Caldwell <blakecaldwell@MacBook-Pro.local>
2019-07-14 22:52:21 -0500

    group chat done

---
+++ 03-express/02-socket.io/02-epic-button-game/server.js
@@ -0,0 +1,37 @@
+// Import express and path modules.
+var express = require("express");
+var path = require( "path");
+// Create the express app.
+var app = express();
+// Define the static folder.
+app.use(express.static(path.join(__dirname, "./static")));
+// Setup ejs templating and define the views folder.
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+// Root route to render the index.ejs view.
+app.get('/', function(req, res) {
+    res.render("index");
+})
+
+var server = app.listen(8000,function(){
+    console.log("listening on port 8000");
+})
+
+var io = require("socket.io").listen(server);
+
+io.sockets.on('connection', function(socket){
+    
+    console.log("Client/socket is connected!");
+    console.log("Client/socked id is: ", socket.id);
+
+    socket.on("button_clicked", function (data){
+        let num = parseInt(data.data);
+        num += 1;
+        socket.emit('updated_count', { response: num });
+    })
+
+    socket.on("reset", function(){
+        socket.emit('updated_count', { response: "0" })
+    })
+})
---
+++ 03-express/02-socket.io/03-real-time-colors-optional/server.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var app = express();
+var server = app.listen(1337);
+var io = require("socket.io")(server);
+var color = "";
+
+//Config
+app.use(express.static(__dirname + "/static"));
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+//Sockets
+io.on("connection", function(socket){
+    console.log("Connected!");
+    socket.emit("launch", {
+        bgc : color
+    });
+    socket.broadcast.emit("launch", {
+        bgc : color
+    });
+
+    socket.on("green_push", function(){
+        socket.emit("green_bg");
+        socket.broadcast.emit("green_bg");
+        color = "green"
+    });
+    socket.on("blue_push", function(){
+        socket.emit("blue_bg");
+        socket.broadcast.emit("blue_bg");
+        color = "blue"
+    });
+    socket.on("pink_push", function(){
+        socket.emit("pink_bg");
+        socket.broadcast.emit("pink_bg");
+        color = "pink"
+    });
+});
+
+//Routes
+app.get("/", function(req, res){
+    console.log("~Root~");
+    res.render("index");
+});
