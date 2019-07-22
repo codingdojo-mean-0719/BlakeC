3c42b0217705d4656eee529a4d1728e30058f845
Blake Caldwell <blakecaldwell@MacBook-Pro.local>
2019-07-22 12:26:02 -0500

    Restful tasks contd completed, resolved issues with DB

--- 05-angular/02-fetch-data/02-restful-task-api/public/src/app/app.component.ts
+++ 05-angular/02-fetch-data/02-restful-task-api/public/src/app/app.component.ts
@@ -1,29 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 import { Observable } from 'rxjs';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'Restful Tasks Continued';
-  tasks = [];
-  task = [];
+  tasks: Task[] = [];
   description: string;
 
-  constructor(private _HTTPSERVICE: HttpService){
-  }
+  constructor(private _HTTPSERVICE: HttpService) {}
   ngOnInit() {
-  this.getTasksFromService();
-  this.description = 'Learn Angular - Understand Services!';
+    this.getTasksFromService();
+    this.description = 'Learn Angular - Understand Services!';
   }
   getTasksFromService() {
     const observable = this._HTTPSERVICE.getTasks();
     observable.subscribe(data => {
       console.log('Got our Data', data);
-      this.tasks = data['tasks'];
+      this.tasks = data;
     });
   }
 }
--- 05-angular/02-fetch-data/02-restful-task-api/public/src/app/http.service.ts
+++ 05-angular/02-fetch-data/02-restful-task-api/public/src/app/http.service.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Observable } from 'rxjs';
+import { Task } from './task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
-  constructor(private _HTTP: HttpClient){
-      // this.getTasks();
+  constructor(private _HTTP: HttpClient) {
+    // this.getTasks();
   }
-  getTasks() {
+  getTasks(): Observable<Task[]> {
     // // our http response is an Observable, store it in a variable
     // const tempObservable = this._HTTP.get('/tasks');
     // // subscribe to the Observable and provide the code we would like to do with our data from the response
     // tempObservable.subscribe(data => console.log('Got our tasks!', data));
-    return this._HTTP.get('/tasks');
+    return this._HTTP.get<Task[]>('/tasks');
   }
 }
-
-
-
